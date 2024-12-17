4de55cdf6245552cbb9e0fd28bb1135fe4e8a0e0
lcs-nhwang <sjnhwang@lcs.on.ca>
2024-12-17 09:04:05 -0500

    Organised folders and create necessary views

---
+++ HobbyCard/Model/Player.swift
@@ -0,0 +1,18 @@
+//
+//  PlayerStructure.swift
+//  HobbyCard
+//
+//  Created by Nicholas Hwang on 17/12/2024.
+//
+
+import SwiftUI
+
+struct PlayerStructure: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PlayerStructure()
+}
---
+++ HobbyCard/Views/CardTabView.swift
@@ -0,0 +1,18 @@
+//
+//  CardTabView.swift
+//  HobbyCard
+//
+//  Created by Nicholas Hwang on 17/12/2024.
+//
+
+import SwiftUI
+
+struct CardTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardTabView()
+}
