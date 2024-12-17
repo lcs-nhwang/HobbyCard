261226bc32e525477009484a9326b2b413f73f52
lcs-nhwang <sjnhwang@lcs.on.ca>
2024-12-17 08:58:47 -0500

    Initial Commit

---
+++ HobbyCard/HobbyCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  HobbyCardApp.swift
+//  HobbyCard
+//
+//  Created by Nicholas Hwang on 17/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct HobbyCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
