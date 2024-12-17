eefefd5b229c1f496aefbd0ad27fcf80dd1e76e6
lcs-nhwang <sjnhwang@lcs.on.ca>
2024-12-17 09:50:36 -0500

    uploaded assets required + a few more alterations to heptagon structure

--- HobbyCard/Shape/Heptagon.swift
+++ HobbyCard/Shape/Heptagon.swift
@@ -18,6 +18,8 @@ struct Heptagon: Shape {
         path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY*0.85))
         path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY*0.15))
         path.addLine(to: CGPoint(x: rect.maxX*0.85, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX*0.15, y: rect.minY))
+        path.closeSubpath()
         
         return path
     }
