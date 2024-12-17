0fc3de8482af6bb934c6a86ee996a763aa9859a0
lcs-nhwang <sjnhwang@lcs.on.ca>
2024-12-17 10:03:36 -0500

    created multiple instances of players

--- HobbyCard/Model/Player.swift
+++ HobbyCard/Model/Player.swift
@@ -11,6 +11,7 @@ struct Player: Identifiable {
     
     let id = UUID()
     let name: String
+    let playerImage: String
     let fcLogo: String
     let position: String
     let mainRating: String
@@ -23,5 +24,7 @@ struct Player: Identifiable {
 }
 
 let playerList = [
-    Player(name: <#T##String#>, fcLogo: <#T##String#>, position: <#T##String#>, mainRating: <#String#>, pace: <#T##String#>, shooting: <#T##String#>, passing: <#T##String#>, dribbling: <#T##String#>, defense: <#T##String#>, physical: <#T##String#>)
+    Player(name: "Jackson", playerImage: "NicolasJackson", fcLogo: "Chelsea", position: "ST", mainRating: "79", pace: "83", shooting: "78", passing: "65", dribbling: "80", defense: "40", physical: "73"),
+    Player(name: "Palmer", playerImage: "ColePalmer", fcLogo: "Chelsea", position: "RM", mainRating: "85", pace: "75", shooting: "82", passing: "83", dribbling: "86", defense: "50", physical: "66"),
+    Player(name: "Salah", playerImage: "MohammedSalah", fcLogo: "LFC", position: "RW", mainRating: "89", pace: "89", shooting: "87", passing: "82", dribbling: "88", defense: "45", physical: "75")
 ]
