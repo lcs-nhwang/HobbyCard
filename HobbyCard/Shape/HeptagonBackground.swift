1a7f88a495c58f17c98033e7902e791ba2b90eb4
lcs-nhwang <sjnhwang@lcs.on.ca>
2024-12-17 09:06:03 -0500

    forgot to add shapes folder, so created one

---
+++ HobbyCard/Shape/HeptagonBackground.swift
@@ -0,0 +1,18 @@
+//
+//  HeptagonBackground.swift
+//  HobbyCard
+//
+//  Created by Nicholas Hwang on 17/12/2024.
+//
+
+import SwiftUI
+
+struct HeptagonBackground: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HeptagonBackground()
+}
